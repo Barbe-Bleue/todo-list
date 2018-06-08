797bf071b083eea86d02ceede6ea4131093c21bf
Barbe-Bleue <screy@icloud.com>
2018-06-08 23:09:34 +0200

    add github api

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,19 +4,36 @@ import Hi from './Hi';
 import Login from '../Login/Login'
 import axios from 'axios'
 
-
 class Home extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      fact: ''
+      fact: '',
+      gitHub: ''
     };
     this.chuck = this.chuck.bind(this);
+    this.getGithub = this.getGithub.bind(this);
   }
 
   chuck(event) {
     axios.get('http://www.chucknorrisfacts.fr/api/get?data=tri:alea;nb:01')
     .then(response => this.setState({fact: response.data[0].fact}));
+  }
+
+  getGithub(event) {
+    axios.get('https://api.github.com/users/Barbe-Bleue')
+    .then(response => {this.setState({gitHub: response.data})})
+  }
+  git() {
+    for(var info in this.state.gitHub) {
+      return (
+        <li className="list-group-item d-flex justify-content-between
+          align-items-center item" key={info}>
+          {this.state.gitHub[info]}
+        </li>
+      );
+    }
+
   }
   render() {
     return(
@@ -40,6 +57,10 @@ class Home extends Component {
           Chuck Norris
         </button>
         <p>{this.state.fact}</p>
+        <button className="btn btn-primary" onClick={this.getGithub}>
+          show Github
+        </button>
+        <ul>{this.git()}</ul>
       </div>
     );
   }
